19ae2613ab411574d2827bfc8f7c62563462fe72
boge <1037195898@qq.com>
2024-02-23 15:16:50 +0800

    Ranges添加注释

--- TSCore/bin/tsCore.d.ts
+++ TSCore/bin/tsCore.d.ts
@@ -1542,18 +1542,39 @@ declare namespace tsCore {
         string(): string;
     }
     export class Range {
-        /** An empty range of values of type Int. */
+        /**
+         * 空范围
+         */
         static EMPTY: Range;
+        /**
+         * 范围的起始值
+         */
         readonly start: number;
+        /**
+         * 范围的结束值（包含）
+         */
         readonly endInclusive: number;
+        /**
+         * 构造函数
+         * @param start 范围的起始值
+         * @param endInclusive 范围的结束值（包含）
+         */
         constructor(start: number, endInclusive: number);
+        /**
+         * 判断给定值是否在范围内
+         * @param value 给定值
+         * @returns 如果在范围内返回true，否则返回false
+         */
         contains(value: number): boolean;
         /**
-         * 检查范围是否为空。
-         *
-         * 如果范围的起始值大于结束值，则该范围为空。
+         * 判断范围是否为空
+         * @returns 如果为空返回true，否则返回false
          */
         isEmpty(): boolean;
+        /**
+         * 将范围转换为数组
+         * @returns 范围对应的数组
+         */
         toArray(): number[];
     }
     export interface IConchRenderObject {
--- TSCore/src/com/ranges/Ranges.ts
+++ TSCore/src/com/ranges/Ranges.ts
@@ -1,29 +1,51 @@
 export class Range {
 
-    /** An empty range of values of type Int. */
+    /**
+     * 空范围
+     */
     static EMPTY = new Range(1, 0)
 
+    /**
+     * 范围的起始值
+     */
     readonly start: number
+
+    /**
+     * 范围的结束值（包含）
+     */
     readonly endInclusive: number
 
+    /**
+     * 构造函数
+     * @param start 范围的起始值
+     * @param endInclusive 范围的结束值（包含）
+     */
     constructor(start: number, endInclusive: number) {
         this.start = start
         this.endInclusive = endInclusive
     }
 
+    /**
+     * 判断给定值是否在范围内
+     * @param value 给定值
+     * @returns 如果在范围内返回true，否则返回false
+     */
     contains(value: number) {
         return this.start <= value && value <= this.endInclusive
     }
 
     /**
-     * 检查范围是否为空。
-     *
-     * 如果范围的起始值大于结束值，则该范围为空。
+     * 判断范围是否为空
+     * @returns 如果为空返回true，否则返回false
      */
     isEmpty() {
         return this.start > this.endInclusive
     }
 
+    /**
+     * 将范围转换为数组
+     * @returns 范围对应的数组
+     */
     toArray(): number[] {
         const arr = []
         for (let i = this.start; i <= this.endInclusive; i++) {
