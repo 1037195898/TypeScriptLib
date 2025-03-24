0dbfebda9311b38ca433316844a60aa6869c4dee
boge <1037195898@qq.com>
2025-03-24 10:46:18 +0800

    fix:调整代码组织顺序
    - 将 content.join("\n\n") 的结果移到 chunkString 前面，优化代码结构
    - 这个修改不影响功能，仅改变了代码的组织方式

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ class GenerateModule {
                 if (this.namespace) {
                     chunkString = 'namespace ' + this.namespace + ' {\n' + chunkString + '\n}'
                 }
-                chunkString += '\n\n' + content.join("\n\n")
+                chunkString = content.join("\n\n") + chunkString
                 chunk.contents = Buffer.from(chunkString)
                 customFun && customFun(chunk)
             }))
